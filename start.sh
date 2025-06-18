fd38dcaa4911348af881a4098265c12c46f4228a
tech327 <divya.technorizen@gmail.com>
2025-06-18 16:36:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@
 # Start action server
 rasa run actions --port 5055 &
 
-# Start rasa server
-rasa run  20250617-115429-principal-channel.tar.gz --enable-api --cors "*" --port 8000
+# Start rasa serverrasa run --enable-api --cors "*" --model models/20250617-115429-principal-channel.tar.gz --port 8000
