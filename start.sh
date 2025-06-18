7f6ec7f36b0a5095b4eddd14fc08fd57c0ba5249
tech327 <divya.technorizen@gmail.com>
2025-06-18 13:38:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Train if no model found
-if [ ! -f "models/*.tar.gz" ]; then
-  echo "Training model..."
-  rasa train
-fi
+
 
 # Start action server
 rasa run actions --port 5055 &
 
 # Start rasa server
-rasa run --model models/20250617-115429-principal-channel.tar.gz --enable-api --cors "*" --port 8000
+rasa run  20250617-115429-principal-channel.tar.gz --enable-api --cors "*" --port 8000
